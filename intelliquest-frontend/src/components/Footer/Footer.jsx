0af072932669bee4e2cab3a94d4417d935130b64
Zachary Polof <ZacharyPolof@gmail.com>
2024-07-27 22:56:13 -0500

    Footer
    updated footer to match styling of Navbar

--- intelliquest-frontend/src/components/Footer/Footer.jsx
+++ intelliquest-frontend/src/components/Footer/Footer.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './Footer.module.css';
 
+// Footer component
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
+        {/* Links section */}
         <div className={styles.links}>
           <a href="#" className={styles.link}>Social Media</a>
           <a href="#" className={styles.link}>Contact Info</a>
           <a href="#" className={styles.link}>Terms Of Service</a>
           <a href="#" className={styles.link}>Policies</a>
         </div>
+        {/* Copyright section */}
         <p className={styles.copyright}>© 2024 IntelliQuest</p>
       </div>
     </footer>
