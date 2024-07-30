7b71c9b7a14147f7d3a1afc9846e0fd1a41f287f
Zachary Polof <ZacharyPolof@gmail.com>
2024-07-29 19:01:15 -0500

    Update seeder.js
    updated seeder.js from initial testing database to included seeds for actual project database needs

--- intelliquest-backend/seeder.js
+++ intelliquest-backend/seeder.js
@@ -1,48 +1,43 @@
+// seeder.js
+require('dotenv').config();
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const User = require('./models/User');
-const bcrypt = require('bcryptjs');
+const Resource = require('./models/Resource');
 
-// Load environment variables
-dotenv.config();
-
-const users = [
+const resources = [
   {
-    name: 'John Doe',
-    email: 'john@example.com',
-    password: '123456', // Add a password field
+    title: 'React Crash Course',
+    description: 'Basics of React, such as components, props, state, and hooks.',
+    url: 'https://www.youtube.com/watch?v=LDB4uaJ87e0&t=2592s',
+    thumbnail: 'https://img.youtube.com/vi/LDB4uaJ87e0/hqdefault.jpg', // Example thumbnail
+    type: 'video',
+    tags: ['sample', 'video'],
   },
   {
-    name: 'Jane Doe',
-    email: 'jane@example.com',
-    password: '654321', // Add a password field
+    title: 'Traversy Media',
+    url: 'https://www.youtube.com/@TraversyMedia',
+    type: 'channel',
+    tags: ['youtube', 'channel', 'traversy'],
   },
+  // Add more resources here following the same structure
 ];
 
-const seedUsers = async () => {
+const seedResources = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected...');
 
-    // Hash passwords before saving
-    for (let user of users) {
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(user.password, salt);
-    }
-
-    await User.deleteMany({});
-    await User.insertMany(users);
-
+    await Resource.insertMany(resources);
     console.log('Data Imported!');
     process.exit();
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error('Error with data import:', error);
     process.exit(1);
   }
 };
 
-seedUsers();
+seedResources();
+
+
 
