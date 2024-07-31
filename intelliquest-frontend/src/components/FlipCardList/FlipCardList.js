b04860ca4776b63b9a2ddb0816b289ce95591ae1
Chelsea Jarvis <chelseajarvis301@icloud.com>
2024-07-31 14:48:49 -0600

    added flipcard components

---
+++ intelliquest-frontend/src/components/FlipCardList/FlipCardList.js
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import FlipCard from '../Flipcard/Flipcard';
+import './FlipCardList.css';
+
+
+
+/// Edit Questions... Make Sub Categroies... 
+// JavaScript, CSS+HTML, Github, APIS, jq, JSON, SQL, NoSQL, React... 
+const FlipCardList = () => {
+  const cards = [
+    { question: 'Question 1', answer: 'Answer 1' },
+    { question: 'Question 2', answer: 'Answer 2' },
+    { question: 'Question 3', answer: 'Answer 3' },
+    { question: 'Question 4', answer: 'Answer 4' },
+    { question: 'Question 5', answer: 'Answer 5' },
+  ];
+
+  return (
+    <div className="flip-card-list">
+      {cards.map((card, index) => (
+        <FlipCard key={index} question={card.question} answer={card.answer} />
+      ))}
+    </div>
+  );
+};
+
+export default FlipCardList;
---
+++ intelliquest-frontend/src/components/Flipcard/Flipcard.jsx
@@ -0,0 +1,31 @@
+﻿import React, { useState } from 'react';
+import './FlipCard.css';
+
+
+// props... reconfig (useState)
+
+import React, { useState } from 'react';
+import './FlipCard.css';
+
+const FlipCard = ({ question, answer }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <div className={`flip-card ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          {question}
+        </div>
+        <div className="flip-card-back">
+          {answer}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlipCard;
