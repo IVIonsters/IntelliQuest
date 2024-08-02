413105ac68bc2956869f641d293f6a3e35096f35
Splash1972 <161398773+Splash1972@users.noreply.github.com>
2024-08-01 22:35:26 -0400

    changed courselist to activites

--- intelliquest-frontend/src/components/Navigation/Navbar.jsx
+++ intelliquest-frontend/src/components/Navigation/Navbar.jsx
@@ -22,7 +22,13 @@ const Navbar = ({ onSearch }) => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>IntelliQuest</div>
-      <form onSubmit={handleSearchSubmit}>
+      <ul className={styles.navLinks}>
+        <li><a href="/">Home</a></li>
+        <li><Link to = "/Activities">Activities</Link></li>
+        <li><Link to ="/About">About</Link></li>
+        <li><Link to ="/Contact">Contact</Link></li>
+      </ul>
+      <form onSubmit={handleSearchSubmit} className={styles.searchForm}>
         <input
           type="text"
           value={searchTerm}
