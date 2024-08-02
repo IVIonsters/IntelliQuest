816d65a0381fa23e563d8c6857cc0de1ccc44fdf
Splash1972 <161398773+Splash1972@users.noreply.github.com>
2024-08-01 23:02:24 -0400

    added images and text to activities page

--- intelliquest-frontend/src/components/Activities/Activities.jsx
+++ intelliquest-frontend/src/components/Activities/Activities.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Activities.module.css';
 import PlaceHolderImage from '../../assets/images/placeholder.jpg';
+import quizImage from '../../assets/images/quiz.jpeg';
+import fcImage from '../../assets/images/fc.jpeg'
+import optImage from '../../assets/images/opt.jpeg'
 
 function Activities() {
     return (
@@ -8,10 +11,24 @@ function Activities() {
             <div className={styles.activitiesInnerContainer}>
                 <div className={styles.gridContainer}>
                     <div className={styles.card}>
-                        <img src={PlaceHolderImage} alt='Quiz' className={styles.cardImage} />
+                        <img src={quizImage} alt='Quiz' className={styles.cardImage} />
                         <div className={styles.cardOverlay}>
-                         <h2>  Quiz </h2> 
-                         <p>Test your knowledge with a randomly generated quiz </p>
+                            <h2>  Quiz </h2>
+                            <p>Test your knowledge with a randomly generated quiz </p>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <img src={fcImage} alt='Quiz' className={styles.cardImage} />
+                        <div className={styles.cardOverlay}>
+                            <h2>  Flash Cards </h2>
+                            <p>Test your knowledge with flash cards </p>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <img src={optImage} alt='Quiz' className={styles.cardImage} />
+                        <div className={styles.cardOverlay}>
+                            <h2>  Code Optimizer </h2>
+                            <p>Learn how to optimize your code </p>
                         </div>
                     </div>
                 </div>
