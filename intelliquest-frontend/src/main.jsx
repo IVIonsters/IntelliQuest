43c142fc26c3e1f018d8e6c62a9fe3ddd692f993
Zachary Polof <ZacharyPolof@gmail.com>
2024-08-05 21:41:24 -0500

    Main Changes
    Updated main.jsx with optimizer route

--- intelliquest-frontend/src/main.jsx
+++ intelliquest-frontend/src/main.jsx
@@ -13,6 +13,7 @@ import Error from './components/Error/Error.jsx';
 import styles from './main.module.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import QuizGenerator from './components/QuizGenerator/QuizGenerator.jsx';
+import CodeOptimizer from './components/CodeOptimizer/CodeOptimizer.jsx';
 
 const router = createBrowserRouter([
   {
@@ -45,12 +46,12 @@ const router = createBrowserRouter([
         element: <Login />
       },
       {
-        path: 'search',
-        element: <SearchResultsPage />
+        path: 'quiz',
+        element: <QuizGenerator />
       },
       {
-        path: 'quiz-generator',
-        element: <QuizGenerator /> // Add this route
+        path: 'optimize',
+        element: <CodeOptimizer />
       }
     ]
   }
