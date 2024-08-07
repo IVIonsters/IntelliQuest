7cb711ac1d003bc690d02ef46e4a3a9e51effe98
Chelsea Jarvis <chelseajarvis3301@icloud.com>
2024-08-06 19:00:34 -0600

    cardlist config

--- intelliquest-frontend/src/components/FlipcardList/FlipcardList.jsx
+++ intelliquest-frontend/src/components/FlipcardList/FlipcardList.jsx
@@ -1,10 +1,10 @@
 ﻿import React, { useEffect, useState } from "react";
-import Flipcard from '../Flipcard';
-
-
+import Flipcard from "../Flipcard";
+// import "./Flipcard.module.css";
+import flipcardsData from "../../utils/flipcards.json";
 
 // import Flipcard from "../Flipcard/Flipcard";
-// import "./Flipcard.module.css";
+
 // import hardCodedFlipcards from "../../utils/flipcards.json";
 
 console.log(flipcards);
@@ -13,14 +13,10 @@ const FlipcardList = ({ topic }) => {
   const [flipcards, setFlipcards] = useState([]);
 
   useEffect(() => {
-  
+    // Filter example data based on the selected topic
     const filteredCards = flipcards.filter((card) => card.topic === topic);
-
     setFlipcards(filteredCards);
   }, [topic]);
-
-
-
 
   return (
     <div>
@@ -32,17 +28,6 @@ const FlipcardList = ({ topic }) => {
         // Display a message if no flipcards are available for the selected topic
         <p>No flipcards available for this topic.</p>
       )}
-
-
-
-
-      {/* {additionalFlipcards.length > 0 && (
-        additionalFlipcards.map((card, index) => (
-          <Flipcard key={index + flipcards.length} question={card.question} answer={card.answer} />
-        ))
-      )}
-
-      <button onClick={fetchAdditionalFlipcards}>Load More Flipcards</button> */}
     </div>
   );
 };
