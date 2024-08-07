02e5a97fefecca2b752fe204ccceba56e9f4b192
Chelsea Jarvis <chelseajarvis3301@icloud.com>
2024-08-07 16:20:05 -0600

    Nav Bar

--- intelliquest-frontend/src/components/Navigation/Navbar.jsx
+++ intelliquest-frontend/src/components/Navigation/Navbar.jsx
@@ -19,9 +19,9 @@ const Navbar = ({ onSearch }) => {
       <ul className={styles.navLinks}>
         <li><a href="/">Home</a></li>
         <li><a href="/courses">Courses</a></li>
+        <li><a href="/Flipcards">Flashcards</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/contact">Contact</a></li>
-        <li><a href="/Flipcards">Flipcards</a></li>
       </ul>
       <form onSubmit={handleSearchSubmit} className={styles.searchForm}>
         <input
