e1435b6043c7cd4b93ddf23c5c35eb1ed032b8f0
Zachary Polof <ZacharyPolof@gmail.com>
2024-08-07 21:51:11 -0500

    Update Login.jsx
    defines login component for user auth
    
    also user input for email/password
    
    setup error handling
    
    redirects to homepage on login

---
+++ intelliquest-frontend/src/components/Auth/Login.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable no-unused-vars */
+// src/components/Auth/Login.jsx
+
+import React, { useState, useContext } from 'react';
+import { AuthContext } from './AuthContext';
+import styles from './Auth.module.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { login } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('https://intelliquestdb.onrender.com/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        setError(data.error);
+      } else {
+        login(data.token);
+      }
+    } catch (error) {
+      setError('An error occurred. Please try again.');
+    }
+  };
+
+  return (
+    <div className={styles.authContainer}>
+      <form onSubmit={handleSubmit} className={styles.authForm}>
+        <h2>Login</h2>
+        {error && <p className={styles.error}>{error}</p>}
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
