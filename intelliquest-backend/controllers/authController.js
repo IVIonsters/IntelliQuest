2852e1e3e1ac72f5724ddb512cc5c5dcc1dbebbe
Zachary Polof <ZacharyPolof@gmail.com>
2024-08-07 19:58:59 -0500

    Login
    Production environment changes

--- intelliquest-backend/controllers/authController.js
+++ intelliquest-backend/controllers/authController.js
@@ -4,39 +4,46 @@ const passport = require("../config/passport");
 const bcrypt = require("bcryptjs");
 const { createToken, verifyToken } = require("../utils/jwtFunctions");
 
-router.post("/api/signup", async (req, res) => {
-    try {
-        const encryptedPassword = await bcrypt.hash(req.body.password, 10);
-        const createdUser = await User.create({ firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, userName: req.body.userName, password: encryptedPassword });
+// Sign up route
+router.post("/signup", async (req, res) => {
+  try {
+    const encryptedPassword = await bcrypt.hash(req.body.password, 10);
+    const createdUser = await User.create({ 
+      firstName: req.body.firstName, 
+      lastName: req.body.lastName, 
+      email: req.body.email, 
+      userName: req.body.userName, 
+      password: encryptedPassword 
+    });
 
-        if (!createdUser) {
-            throw Error("User not created successfully.");
-        }
-
-        const { password, ...userWithoutPassword } = createdUser.toJSON();
-
-        const token = createToken(userWithoutPassword);
-
-        res.status(201).json({ message: "User created successfully.", token });
-
-    } catch (error) {
-        console.log({ error });
-        res.status(500).json(error);
+    if (!createdUser) {
+      throw new Error("User not created successfully.");
     }
+
+    const { password, ...userWithoutPassword } = createdUser.toJSON();
+    const token = createToken(userWithoutPassword);
+
+    res.status(201).json({ message: "User created successfully.", token });
+
+  } catch (error) {
+    console.log({ error });
+    res.status(500).json(error);
+  }
 });
 
-router.post('/api/login', (req, res, next) => {
-    passport.authenticate('local', (err, user) => {
-        if (err) return next(err);
+// Login route
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user) => {
+    if (err) return next(err);
 
-        if (!user) return res.status(401).json({ message: "User not found." });
+    if (!user) return res.status(401).json({ message: "User not found." });
 
-        const { password, ...userWithoutPassword } = user.toJSON();
+    const { password, ...userWithoutPassword } = user.toJSON();
+    const token = createToken(userWithoutPassword);
 
-        const token = createToken(userWithoutPassword);
-
-        return res.json({ token });
-    })(req, res, next);
+    return res.json({ token });
+  })(req, res, next);
 });
 
 module.exports = router;
+
--- intelliquest-frontend/src/components/Auth/Signup.jsx
+++ intelliquest-frontend/src/components/Auth/Signup.jsx
@@ -1,104 +1,72 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { setDataLocalStorage } from '../../utils/browserStorage';
 import styles from './Signup.module.css';
 import brainImage from '../../assets/images/brain_image.png';
-import { useState } from 'react';
-import { setDataLocalStorage } from '../../utils/browserStorage';
-import { useNavigate } from 'react-router-dom';
 
 function Signup() {
-    const [user, setUser] = useState({ firstName: "", lastName: "", email: "", userName: "", password: "" });
-    const [error, setError] = useState("");
-    const AUTH_TOKEN_NAME = import.meta.env.VITE_AUTH_TOKEN_NAME;
-    let navigate = useNavigate();
+  const [user, setUser] = useState({ firstName: "", lastName: "", email: "", userName: "", password: "" });
+  const [error, setError] = useState("");
+  const AUTH_TOKEN_NAME = import.meta.env.VITE_AUTH_TOKEN_NAME;
+  let navigate = useNavigate();
 
-    async function handleClick(e) {
-        try {
-            e.preventDefault();
+  async function handleClick(e) {
+    e.preventDefault();
+    const baseUrl = import.meta.env.VITE_API_URL;
+    const url = `${baseUrl}/api/signup`;
 
-            // Update URL to point to Render backend
-            const baseUrl = import.meta.env.VITE_API_URL || 'https://intelliquestdb.onrender.com/';
-            const url = `${baseUrl}/api/signup`; // Ensure baseUrl ends with a slash and add slash before 'api'
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      });
 
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ firstName: user.firstName, lastName: user.lastName, email: user.email, userName: user.userName, password: user.password })
-            });
-
-            if (!response.ok) {
-                const apiError = await response.json();
-                setError(apiError.message || apiError);
-            } else {
-                const token = await response.json();
-                if (!token) {
-                    setError("Sign Up Unsuccessful");
-                } else {
-                    setError("");
-                    setDataLocalStorage(AUTH_TOKEN_NAME, token.token);
-                    setUser({ firstName: "", lastName: "", email: "", userName: "", password: "" });
-                    navigate("/home");
-                }
-            }
-        } catch (error) {
-            setError(error.message);
-        }
+      if (!response.ok) {
+        const apiError = await response.json();
+        setError(apiError.message);
+      } else {
+        const { token } = await response.json();
+        setError("");
+        setDataLocalStorage(AUTH_TOKEN_NAME, token);
+        setUser({ firstName: "", lastName: "", email: "", userName: "", password: "" });
+        navigate("/home");
+      }
+    } catch (error) {
+      setError(error.message);
     }
+  }
 
-    function handleFirstNameOnChange(e) {
-        e.preventDefault();
-        setUser({ ...user, firstName: e.target.value });
-    }
-
-    function handleLastNameOnChange(e) {
-        e.preventDefault();
-        setUser({ ...user, lastName: e.target.value });
-    }
-
-    function handleEmailOnChange(e) {
-        e.preventDefault();
-        setUser({ ...user, email: e.target.value });
-    }
-
-    function handleUserNameOnChange(e) {
-        e.preventDefault();
-        setUser({ ...user, userName: e.target.value });
-    }
-
-    function handlePasswordOnChange(e) {
-        e.preventDefault();
-        setUser({ ...user, password: e.target.value });
-    }
-
-    return (
-        <div className={styles.signup}>
-            <div className={styles.container}>
-                <div className={styles.sidebar}><img src={brainImage} alt="" />
-                    <h1>Welcome to IntelliQuest!</h1>
-                    <h2>Whether you're a beginner or looking to sharpen your skills, we're here to guide you every step of the way. Let's build something amazing together!</h2>
-                </div>
-                <form className={styles.form}>
-                    <h1>Sign Up</h1>
-                    <p className={styles.error}>{error}</p>
-                    <h2>First Name:</h2>
-                    <input onChange={handleFirstNameOnChange} className={styles.entryBox} name="firstName" type="text" placeholder='Enter first name:' value={user.firstName} />
-                    <h2>Last Name:</h2>
-                    <input onChange={handleLastNameOnChange} className={styles.entryBox} name="lastName" type="text" placeholder='Enter last name:' value={user.lastName} />
-                    <h2>Email:</h2>
-                    <input onChange={handleEmailOnChange} className={styles.entryBox} name="email" type="text" placeholder='Enter email:' value={user.email} />
-                    <h2>User Name:</h2>
-                    <input onChange={handleUserNameOnChange} className={styles.entryBox} name="userName" type="text" placeholder='Enter user name:' value={user.userName} />
-                    <h2>Password:</h2>
-                    <input onChange={handlePasswordOnChange} className={styles.entryBox} name="password" type="password" placeholder='Enter password:' value={user.password} />
-                    <button onClick={handleClick} className={styles.button} type="submit">Sign Up</button>
-                    <div className={styles.loginLink}>
-                        <h2>Already have an account?</h2>
-                        <h2><a href="/login">Log In</a></h2>
-                    </div>
-                </form>
-            </div>
+  return (
+    <div className={styles.signup}>
+      <div className={styles.container}>
+        <div className={styles.sidebar}><img src={brainImage} alt="" />
+          <h1>Welcome to IntelliQuest!</h1>
+          <h2>Whether you're a beginner or looking to sharpen your skills, we're here to guide you every step of the way. Let's build something amazing together!</h2>
         </div>
-    );
+        <form className={styles.form}>
+          <h1>Sign Up</h1>
+          <p className='error'>{error}</p>
+          <h2>First Name:</h2>
+          <input onChange={(e) => setUser({ ...user, firstName: e.target.value })} className={styles.entryBox} name="firstName" type="text" placeholder='Enter first name:' value={user.firstName} />
+          <h2>Last Name:</h2>
+          <input onChange={(e) => setUser({ ...user, lastName: e.target.value })} className={styles.entryBox} name="lastName" type="text" placeholder='Enter last name:' value={user.lastName} />
+          <h2>Email:</h2>
+          <input onChange={(e) => setUser({ ...user, email: e.target.value })} className={styles.entryBox} name="email" type="text" placeholder='Enter email:' value={user.email} />
+          <h2>User Name:</h2>
+          <input onChange={(e) => setUser({ ...user, userName: e.target.value })} className={styles.entryBox} name="userName" type="text" placeholder='Enter user name:' value={user.userName} />
+          <h2>Password:</h2>
+          <input onChange={(e) => setUser({ ...user, password: e.target.value })} className={styles.entryBox} name="password" type="password" placeholder='Enter password:' value={user.password} />
+          <button onClick={handleClick} className={styles.button} type="submit">Sign Up</button>
+          <div className={styles.loginLink}>
+            <h2>Already have an account?</h2>
+            <h2><a href="/login">Log In</a></h2>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
+
