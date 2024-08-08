9deea6d2fb3daaa0d2dfb8e6f14ed21f8d659ebd
Zachary Polof <ZacharyPolof@gmail.com>
2024-08-07 21:07:14 -0500

    Update authController.js
    added login route to controller

--- intelliquest-backend/controllers/authController.js
+++ intelliquest-backend/controllers/authController.js
@@ -27,4 +27,29 @@ router.post('/api/signup', async (req, res) => {
   }
 });
 
+// Login Route
+router.post('/api/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const token = createToken({ id: user._id, email: user.email });
+
+    res.status(200).json({ message: 'Login successful', token });
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(500).json({ error: 'Error logging in' });
+  }
+});
+
+module.exports = router;
