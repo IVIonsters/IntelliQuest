bf3be97612785d9bc24014a4b8bdafdeea8a2c5c
Zachary Polof <ZacharyPolof@gmail.com>
2024-08-09 17:35:38 -0500

    Update AuthContext.jsx
    updated for token in local storage and to decode, on logout token is removed from storage

--- intelliquest-frontend/src/components/Auth/AuthContext.jsx
+++ intelliquest-frontend/src/components/Auth/AuthContext.jsx
@@ -8,14 +8,27 @@ import { jwtDecode } from "jwt-decode";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children, navigate }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedUser = jwtDecode(token); // Decode the token to get user data
+        return { ...decodedUser, token };
+      } catch (error) {
+        console.error('Failed to decode token:', error);
+        return null;
+      }
+    }
+    return null;
+  });
 
   const login = (token) => {
     if (typeof token === 'string') {
       try {
         const decodedUser = jwtDecode(token); // Decode the token to get user data
         console.log('Decoded User:', decodedUser); // Check if the token is correctly decoded
-        setUser(decodedUser);
+        setUser({ ...decodedUser, token });
+        localStorage.setItem('token', token);
       } catch (error) {
         console.error('Failed to decode token:', error);
       }
@@ -28,7 +41,7 @@ const AuthProvider = ({ children, navigate }) => {
   const logout = () => {
     setUser(null);
     // Remove user data from localStorage
-    localStorage.removeItem('user');
+    localStorage.removeItem('token');
     navigate('/login');
   };
 
@@ -40,4 +53,3 @@ const AuthProvider = ({ children, navigate }) => {
 };
 
 export { AuthProvider, AuthContext };
-
