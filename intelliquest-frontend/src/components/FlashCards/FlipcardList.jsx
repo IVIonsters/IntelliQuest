3a60ac0bd7960bd0df130720f7174cdca7f422fa
Zachary Polof <ZacharyPolof@gmail.com>
2024-08-10 00:02:21 -0500

    Flashcard
    integrating flash card from another branch

---
+++ intelliquest-frontend/src/components/FlashCards/FlipcardList.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+// FlashCards/FlipcardList.jsx
+import React from 'react';
+import Flipcard from './Flipcard';
+import styles from './FlipcardList.module.css';
+
+const FlipcardList = ({ flipcards }) => {
+    return (
+        <div className={styles.flipcardList}>
+            {flipcards.map((card, index) => (
+                <Flipcard key={index} card={card} />
+            ))}
+        </div>
+    );
+};
+
+export default FlipcardList;
