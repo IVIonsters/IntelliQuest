de2877fa2ca2e2cb18e5e2f96dce5e115b1f4b8d
Zachary Polof <ZacharyPolof@gmail.com>
2024-08-10 20:29:55 -0500

    Markdown
    Restructured for editable markdown

--- intelliquest-frontend/src/components/UserDashboard/UserDashboard.jsx
+++ intelliquest-frontend/src/components/UserDashboard/UserDashboard.jsx
@@ -31,6 +31,58 @@ const UserDashboard = () => {
         };
   });
 
+  const [markdownContent, setMarkdownContent] = useState(() => {
+    const savedMarkdownContent = localStorage.getItem('markdownContent');
+    return savedMarkdownContent
+      ? savedMarkdownContent
+      : `
+  # Hello,👋🏻 I appreciate you stopping by! Check out my projects!
+  <br>
+
+  [![Static Badge](https://img.shields.io/badge/Zachary-IVIonsters_Designs-teal)](https://ivionsters.github.io/Journeys-End/)
+
+  <br>  
+  ###  Exploring the beauty in the world through technology.🌎
+  <br>
+  <br>
+  I have a passion for programming and am constantly looking for ways to expand my knowledge! Relentless in my pursuit to become more knowledgeable about my craft! I enjoy crafting dynamically eventful websites and projects. Real-world application and focus are what I strive for in my project concepts. Let's connect and build something to change the world! ☄
+  
+  <br>
+  <br>
+  **Let's Connect:📱**
+  <a href="https://github.com/IVIonsters"><img alt="Github Url" src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white"></a> 
+  <a href="mailto:zacharypolof@gmail.com"><img alt="Email Me!" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"></a> 
+  <a href="https://www.instagram.com/zpolof/"><img alt="Instagram Link" src="https://img.shields.io/badge/Instagram-%23E4405F.svg?style=for-the-badge&logo=Instagram&logoColor=white"></a> 
+  <a href="https://www.facebook.com/zachary.polof"><img alt="Facebook Profile" src="https://img.shields.io/badge/Facebook-%231877F2.svg?style=for-the-badge&logo=Facebook&logoColor=white"></a>
+  <br>
+  <br>
+  <br>
+  
+  **📖 I’m currently learning ...** Advanced JavaScript, Node, React.
+  <br>
+  <br>
+  <br>
+  **🌐 Languages and Tools**
+  
+  <img align="left" alt="Github" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" /> 
+  <img align="left" alt="HTML" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" /> 
+  <img align="left" alt="CSS" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" /> 
+  <img align="left" alt="JavaScript" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" /> 
+  <img align="left" alt="Bootstrap" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" /> 
+  <img align="left" alt="jQuery" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original.svg" /> 
+  <img align="left" alt="GIT" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" /> 
+  <img align="left" alt="NPM" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg" /> 
+  <img align="left" alt="Express" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" /> 
+  <img align="left" alt="Node" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" /> 
+  <img align="left" alt="SQL" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" /> 
+  <img align="left" alt="handlebars" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/handlebars/handlebars-original-wordmark.svg" /> 
+  <img align="left" alt="MongoDB" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg" /> 
+  <img align="left" alt="Mongoose" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original-wordmark.svg" /> 
+  <img align="left" alt="Docker" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg" /> 
+  <img align="left" alt="React" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" /> 
+  `;
+  });
+
   useEffect(() => {
     // Fetch random resources from the database
     const fetchResources = async () => {
@@ -229,69 +281,43 @@ const UserDashboard = () => {
         </div>
 
         <div className={styles.readmeContainer}>
-          <ReactMarkdown rehypePlugins={[rehypeRaw]}>{`
-  # Hello,👋🏻 I appreciate you stopping by! Check out my projects!
-  <br>
-
-  [![Static Badge](https://img.shields.io/badge/Zachary-IVIonsters_Designs-teal)](https://ivionsters.github.io/Journeys-End/)
-
-<br>  
-  ###  Exploring the beauty in the world through technology.🌎
-<br>
-<br>
-  I have a passion for programming and am constantly looking for ways to expand my knowledge! Relentless in my pursuit to become more knowledgeable about my craft! I enjoy crafting dynamically eventful websites and projects. Real-world application and focus are what I strive for in my project concepts. Let's connect and build something to change the world! ☄
-
-  <br>
-  <br>
-  **Let's Connect:📱**
-  <a href="https://github.com/IVIonsters"><img alt="Github Url" src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white"></a> 
-  <a href="mailto:zacharypolof@gmail.com"><img alt="Email Me!" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"></a> 
-  <a href="https://www.linkedin.com/in/zacharypolof/"><img alt="LinkedIn Profile" src="https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white"></a> 
-  <a href="https://www.instagram.com/zpolof/"><img alt="Instagram Link" src="https://img.shields.io/badge/Instagram-%23E4405F.svg?style=for-the-badge&logo=Instagram&logoColor=white"></a> 
-  <a href="https://www.facebook.com/zachary.polof"><img alt="Facebook Profile" src="https://img.shields.io/badge/Facebook-%231877F2.svg?style=for-the-badge&logo=Facebook&logoColor=white"></a>
-<br>
-<br>
-<br>
-
-  **📖 I’m currently learning ...** Advanced JavaScript, Node, React.
-<br>
-<br>
-<br>
-  **🌐 Languages and Tools**
-
-  <img align="left" alt="Github" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" /> 
-  <img align="left" alt="HTML" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" /> 
-  <img align="left" alt="CSS" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" /> 
-  <img align="left" alt="JavaScript" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" /> 
-  <img align="left" alt="Bootstrap" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" /> 
-  <img align="left" alt="jQuery" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original.svg" /> 
-  <img align="left" alt="GIT" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" /> 
-  <img align="left" alt="NPM" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg" /> 
-  <img align="left" alt="Express" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" /> 
-  <img align="left" alt="Node" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" /> 
-  <img align="left" alt="SQL" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" /> 
-  <img align="left" alt="handlebars" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/handlebars/handlebars-original-wordmark.svg" /> 
-  <img align="left" alt="MongoDB" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg" /> 
-  <img align="left" alt="Mongoose" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original-wordmark.svg" /> 
-  <img align="left" alt="Docker" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg" /> 
-  <img align="left" alt="React" width="30px" style="padding-right:10px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" /> 
-          `}</ReactMarkdown>
+          {isEditing.markdown ? (
+            <>
+              <textarea
+                value={markdownContent}
+                onChange={(e) => setMarkdownContent(e.target.value)}
+                className={styles.textarea}
+              />
+              <div className={styles.editButtons}>
+                <button onClick={() => {
+                  setIsEditing({ ...isEditing, markdown: false });
+                  localStorage.setItem('markdownContent', markdownContent);
+                }}>Save</button>
+                <button onClick={() => setIsEditing({ ...isEditing, markdown: false })}>Cancel</button>
+              </div>
+            </>
+          ) : (
+            <>
+              <ReactMarkdown rehypePlugins={[rehypeRaw]}>{markdownContent}</ReactMarkdown>
+              <FaPencilAlt className={styles.editIcon} onClick={() => handleEditClick('markdown')} />
+            </>
+          )}
         </div>
       </div>
 
       <div className={styles.resourcesContainer}>
-      <h3>Favorite Resources</h3>
-      <div className={styles.resourceCards}>
-        {resources.map((resource, index) => (
-          <div key={index} className={styles.resourceCard}>
-            <img src={resource.thumbnail} alt={resource.title} />
-            <h4>{resource.title}</h4>
-            <p>{resource.description}</p>
-            <a href={resource.link} target="_blank" rel="noopener noreferrer">Visit</a>
-          </div>
-        ))}
-  </div>
-</div>
+        <h3>Favorite Resources</h3>
+        <div className={styles.resourceCards}>
+          {resources.map((resource, index) => (
+            <div key={index} className={styles.resourceCard}>
+              <img src={resource.thumbnail} alt={resource.title} />
+              <h4>{resource.title}</h4>
+              <p>{resource.description}</p>
+              <a href={resource.link} target="_blank" rel="noopener noreferrer">Visit</a>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
