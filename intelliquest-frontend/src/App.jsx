9b217f565ccb1446686990112aac02a360347843
Zachary Polof <ZacharyPolof@gmail.com>
2024-09-18 22:03:34 -0500

    Particles
    added react particles and styled particles background

--- intelliquest-frontend/src/App.jsx
+++ intelliquest-frontend/src/App.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react'
 import './App.css'
+import ParticlesBg from 'particles-bg'
+
+// ParticlesBg options
+const particlesOptions = {
+  className: 'particles',
+  type: 'cobweb',
+  bg: true,
+  num: 400,
+  color: '#FFFFFF',
+}
+
 
 function App() {
 
   return (
-<p>Hello, World</p>
+    <div className='App'>
+      <p>Hello, World</p>
+      <ParticlesBg {...particlesOptions} />
+    </div>
   )
 }
 
